d5c2b9b51de3c4efb1ce9ee36b78de7e2902427d
Efield44444235364 <efi3lx@gmail.com>
2025-10-23 23:35:09 +0700

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,40 @@
+local folderPath = "Kawnew"
+local imageName = "Kawnew_Halloween.jpg"
+local fullPath = folderPath .. "/" .. imageName
+local imageUrl = "https://raw.githubusercontent.com/Efield44444235364/KawnewLogo/refs/heads/main/1358473198_image_473660_1761229658486(1).jpg" -- 🔁 เปลี่ยนเป็นลิงก์จริงของรูป
+
+-- รองรับทั้ง getsynasset และ getcustomasset
+local getAsset = getsynasset or getcustomasset or function() return "" end
+
+-- ตรวจสอบไฟล์
+local function fileExists(path)
+    local ok, result = pcall(function()
+        return readfile(path)
+    end)
+    return ok and result ~= nil
+end
+
+-- ถ้าไม่มีโฟลเดอร์ Kawnew ให้สร้างก่อน
+if not isfolder(folderPath) then
+    makefolder(folderPath)
+end
+
+-- ถ้าไฟล์ยังไม่มี ให้โหลดก่อน
+if not fileExists(fullPath) then
+    local success, result = pcall(function()
+        writefile(fullPath, game:HttpGet(imageUrl))
+    end)
+
+    if not success then
+        warn("Photo Cant load: " .. tostring(result))
+        return
+    end
+end
+
+-- แสดง Notification พร้อมรูป
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Kawnew X Halloween 🎃",
+    Text = "โหลดรูปเสร็จแล้ว!",
+    Icon = getAsset(fullPath),
+    Duration = 3
+})
